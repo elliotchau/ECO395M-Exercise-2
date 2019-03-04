f134f3db71dc91752ade7edd985cc27b19659c69
Elliot Chau <elliot.chau@utexas.edu>
2019-03-04 00:41:56 -0600

    Update

--- Problem 1/saratoga_lm.R
+++ Problem 1/saratoga_lm.R
@@ -76,12 +76,12 @@ rmse_vals = do(100)*{
   lm1 = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
                         fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=SaratogaHouses)
   
-  lm_boom = lm(price ~ lotSize + age + pctCollege + 
+  lm2 = lm(price ~ lotSize + age + pctCollege + 
                  fireplaces + rooms + heating + fuel + centralAir +
                  bedrooms*rooms + bathrooms*rooms + 
                  bathrooms*livingArea, data=saratoga_train)
   
-  lm_biggerboom = lm(price ~ lotSize + landValue + waterfront + lotSize*livingArea 
+  lm3 = lm(price ~ lotSize + landValue + waterfront + lotSize*livingArea 
                      + age + ageSq + bedrooms*bathrooms + heating + centralAir + livingArea + newConstruction
                      + rooms*bedrooms + rooms*bathrooms + rooms*heating + landValue*age
                      + pctCollege + valueSqFt, data=saratoga_train)
@@ -89,21 +89,24 @@ rmse_vals = do(100)*{
   
   # predict on this testing set
   yhat_test1 = predict(lm1, saratoga_test)
-  yhat_testboom = predict(lm_boom, saratoga_test)
-  yhat_testbiggerboom = predict(lm_biggerboom, saratoga_test)
-  c(rmse(saratoga_test$price, yhat_test2),
-    rmse(saratoga_test$price, yhat_testboom),
-    rmse(saratoga_test$price, yhat_testbiggerboom))
+  yhat_test2 = predict(lm2, saratoga_test)
+  yhat_test3 = predict(lm3, saratoga_test)
+  c(rmse(saratoga_test$price, yhat_test1),
+    rmse(saratoga_test$price, yhat_test2),
+    rmse(saratoga_test$price, yhat_test3))
 }
 
 rmse_vals
 colMeans(rmse_vals)
 
-#applying the biggerboom model to every observation
-SaratogaHouses$prediction <- predict(lm_biggerboom)
 
-###### PART 2 ######
+############################### PART 2 ###########################################################
+library(FNN)
+library(foreach)
+
 ###Turn your model into a better performing KNN model 
+ 
+n = nrow(SaratogaHouses)
 n_train = round(0.8*n)  # round to nearest integer
 n_test = n - n_train
 train_cases = sample.int(n, n_train, replace=FALSE)
@@ -111,12 +114,59 @@ test_cases = setdiff(1:n, train_cases)
 saratoga_train = SaratogaHouses[train_cases,]
 saratoga_test = SaratogaHouses[test_cases,]
 
-# Now separate the training and testing sets into features (X) and outcome (y)
-X_train = select(saratoga_train, yhat_testbiggerboom)
-y_train = select(n_train, price)
 
-X_test = select(saratoga_test, lotSize + landValue + waterfront + lotSize*livingArea 
-                + age + ageSq + bedrooms*bathrooms + heating + centralAir + livingArea + newConstruction
-                + rooms*bedrooms + rooms*bathrooms + rooms*heating + landValue*age
-                + pctCollege + valueSqFt, data=saratoga_train)
-y_test = select(n_test, price)
+# Fit to the training data
+lm1 = lm(price ~ lotSize + bedrooms + bathrooms, data=saratoga_train)
+lm2 = lm(price ~ . - sewer - waterfront - landValue - newConstruction, data=saratoga_train)
+lm3 = lm(price ~ lotSize + landValue + waterfront + lotSize*livingArea
+         + age + ageSq + bedrooms*bathrooms + heating + centralAir + livingArea + newConstruction
+         + rooms*bedrooms + rooms*bathrooms + rooms*heating + landValue*age
+         + pctCollege + valueSqFt, data=saratoga_train) 
+
+# Predictions out of sample
+yhat_test1 = predict(lm1, saratoga_test)
+yhat_test2 = predict(lm2, saratoga_test)
+yhat_test3 = predict(lm3, saratoga_test)
+
+
+rmse = function(y, yhat) {
+  sqrt( mean( (y - yhat)^2 ) )
+}
+
+# Root mean-squared prediction error
+rmse(saratoga_test$price, yhat_test1)
+rmse(saratoga_test$price, yhat_test2)
+rmse(saratoga_test$price, yhat_test3)
+
+  
+# construct the training and test-set feature matrices
+# note the "-1": this says "don't add a column of ones for the intercept"
+Xtrain = model.matrix(~ . - (price + sewer + fuel + fireplaces) - 1, data=saratoga_test) 
+Xtest = model.matrix(~ . - (price + sewer + fuel + fireplaces) - 1, data=saratoga_test)
+
+
+# training and testing set responses
+ytrain = saratoga_train$price
+ytest = saratoga_test$price
+
+# now rescale:
+scale_train = apply(Xtrain, 2, sd)  # calculate std dev for each column
+Xtilde_train = scale(Xtrain, scale = scale_train)
+Xtilde_test = scale(Xtest, scale = scale_train)  # use the training set scales!
+
+  K = 10
+
+  # fit the model
+  knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=K)
+
+  # calculate test-set performance
+  rmse(ytest, knn_model$pred)
+  rmse(ytest, yhat_test3)
+
+  k_grid = exp(seq(log(2), log(300), length=100)) %>% round %>% unique
+  rmse_grid = foreach(K = k_grid, .combine='c') %do% {
+    knn_model = knn.reg(Xtilde_train, Xtilde_test, ytrain, k=K)
+    rmse(ytest, knn_model$pred)
+  }
+  
+plot(k_grid, rmse_grid, log='x')
