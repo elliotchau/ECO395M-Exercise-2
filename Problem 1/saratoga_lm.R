35b122c0c30250db8bfe206ee7539894f9a3bd5c
Elliot Chau <elliot.chau@utexas.edu>
2019-03-03 20:18:38 -0600

    Update

--- Problem 1/saratoga_lm.R
+++ Problem 1/saratoga_lm.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(mosaic)
 data(SaratogaHouses)
-
 summary(SaratogaHouses)
 
-# Baseline model
-lm_small = lm(price ~ bedrooms + bathrooms + lotSize, data=SaratogaHouses)
+###### PART 1 ######
+  ###Build a better model than "lm_medium" in the start script
+  ###Any particularly strong drivers of house price?
 
-# 11 main effects
+# baseline model
 lm_medium = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
                  fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=SaratogaHouses)
 
@@ -20,14 +20,14 @@ lm_medium2 = lm(price ~ . - sewer - waterfront - landValue - newConstruction, da
 coef(lm_medium)
 coef(lm_medium2)
 
-# All interactions
-# the ()^2 says "include all pairwise interactions"
-lm_big = lm(price ~ (. - sewer - waterfront - landValue - newConstruction)^2, data=SaratogaHouses)
-
-
-####
 # Compare out-of-sample predictive performance
-####
+#### Create some new variables to include in the model
+  #intrinsic land value to size of living area ($/sq ft)
+SaratogaHouses$valueSqFt <- SaratogaHouses$landValue/SaratogaHouses$livingArea
+  # age squared; older houses significantly drop in value
+SaratogaHouses$ageSq <- SaratogaHouses$age*SaratogaHouses$age
+
+
 
 # Split into training and testing sets
 n = nrow(SaratogaHouses)
@@ -39,14 +39,28 @@ saratoga_train = SaratogaHouses[train_cases,]
 saratoga_test = SaratogaHouses[test_cases,]
 
 # Fit to the training data
-lm1 = lm(price ~ lotSize + bedrooms + bathrooms, data=saratoga_train)
-lm2 = lm(price ~ . - sewer - waterfront - landValue - newConstruction, data=saratoga_train)
-lm3 = lm(price ~ (. - sewer - waterfront - landValue - newConstruction)^2, data=saratoga_train)
+# The baseline model
+lm1 = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
+     fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=SaratogaHouses)
+
+# Adding land value and interactions with rooms and bedrooms (from class)
+  # The logic is that bathrooms should match bedrooms so you don't have to wait forever
+  # Also heated rooms 
+lm2 = lm(price ~ lotSize + landValue + waterfront + newConstruction + bedrooms*bathrooms + heating 
+         + fuel + pctCollege + rooms*bedrooms + rooms*bathrooms 
+         + rooms*heating + livingArea, data=saratoga_train)
+
+# Add some interactions that definitely determine value like valueSqFt, ageSq, landValue*age
+lm3 = lm(price ~ lotSize + landValue + waterfront + lotSize*livingArea #intrinsic land qualities
+        + age + ageSq + bedrooms*bathrooms + heating + centralAir + livingArea + newConstruction #house qualities
+        + rooms*bedrooms + rooms*bathrooms + rooms*heating + landValue*age #interacting house qualities
+        + pctCollege + valueSqFt, data=saratoga_train) #neighborhood qualities
 
 # Predictions out of sample
 yhat_test1 = predict(lm1, saratoga_test)
 yhat_test2 = predict(lm2, saratoga_test)
 yhat_test3 = predict(lm3, saratoga_test)
+
 
 rmse = function(y, yhat) {
   sqrt( mean( (y - yhat)^2 ) )
@@ -54,10 +68,15 @@ rmse = function(y, yhat) {
 
 # Root mean-squared prediction error
 rmse(saratoga_test$price, yhat_test1)
+  ### RMSE = 66,555
 rmse(saratoga_test$price, yhat_test2)
+  ### RMSE = 61,401
 rmse(saratoga_test$price, yhat_test3)
+  ### rmse = 61,092
 
 
+###### PART 2 ######
+  ###Turn your model into a better performing KNN model 
 # easy averaging over train/test splits
 library(mosaic)
 
@@ -72,18 +91,22 @@ rmse_vals = do(100)*{
   saratoga_test = SaratogaHouses[test_cases,]
   
   # fit to this training set
-  lm2 = lm(price ~ . - sewer - waterfront - landValue - newConstruction, data=saratoga_train)
+  lm1 = lm(price ~ lotSize + age + livingArea + pctCollege + bedrooms + 
+                        fireplaces + bathrooms + rooms + heating + fuel + centralAir, data=SaratogaHouses)
   
   lm_boom = lm(price ~ lotSize + age + pctCollege + 
                  fireplaces + rooms + heating + fuel + centralAir +
                  bedrooms*rooms + bathrooms*rooms + 
                  bathrooms*livingArea, data=saratoga_train)
   
-  lm_biggerboom = lm(price ~ lotSize + landValue + waterfront + newConstruction + bedrooms*bathrooms + heating + fuel + pctCollege + rooms*bedrooms + rooms*bathrooms + rooms*heating + livingArea, data=saratoga_train)
+  lm_biggerboom = lm(price ~ lotSize + landValue + waterfront + lotSize*livingArea 
+                     + age + ageSq + bedrooms*bathrooms + heating + centralAir + livingArea + newConstruction
+                     + rooms*bedrooms + rooms*bathrooms + rooms*heating + landValue*age
+                     + pctCollege + valueSqFt, data=saratoga_train)
   
   
   # predict on this testing set
-  yhat_test2 = predict(lm2, saratoga_test)
+  yhat_test1 = predict(lm1, saratoga_test)
   yhat_testboom = predict(lm_boom, saratoga_test)
   yhat_testbiggerboom = predict(lm_biggerboom, saratoga_test)
   c(rmse(saratoga_test$price, yhat_test2),
